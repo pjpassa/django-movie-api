8bc65bab26c8a8cb595f21d96c9edcf4caf6c897
PJ Passalacqua <pjpassa@gmail.com>
2015-07-14 14:23:43 -0400

    get working

--- crud/api/views.py
+++ crud/api/views.py
@@ -1,3 +1,16 @@
+from django.core.serializers import serialize
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from movie.models import Movie
+
+
+def get_and_create_movies(request):
+    if request.method == "GET":
+        return HttpResponse(serialize('json', Movie.objects.all()), content_type="application/json")
+    if request.method == "POST":
+        pass
+    else:
+        return HttpResponse('[]', content_type="application/json", status=403)
--- crud/crud/urls.py
+++ crud/crud/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from api.views import get_and_create_movies
 
 from movie.views import MovieListView, MovieCreateView, MovieDeleteView, MovieDetailView
 
 urlpatterns = [
+    url(r'^api/movies/', get_and_create_movies, name="api_all_movies"),
     url(r'^movie_list/', MovieListView.as_view(), name="movie_list"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^create_movie/', MovieCreateView.as_view(), name="create_movie"),
     url(r'^delete_movie/(?P<pk>\d+)/',MovieDeleteView.as_view(), name="delete_movie"),
-    url(r'^movie_detail/(?P<pk>\d+)/',MovieDetailView.as_view(), name="movie_detail")
-
+    url(r'^movie_detail/(?P<pk>\d+)/',MovieDetailView.as_view(), name="movie_detail"),
 ]
